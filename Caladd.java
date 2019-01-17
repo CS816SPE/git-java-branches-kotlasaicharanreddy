68dc197fab94876721c405482cb9d63775687d6e
kotlasaicharanreddy <kotlasaicharan@yahoo.com>
2019-01-17 09:59:12 +0530

    addition module

---
+++ Caladd.java
@@ -0,0 +1,31 @@
+import java.util.Scanner.*;
+public class Caladd
+{
+	public static void main(String args[])
+	{
+		float a,b,res;
+		char choice,ch;
+		Scanner scan = new Scanner(System.in);
+		do
+		{
+			System.out.print("\n-----------------\n");
+			System.out.print("1. Add two numbers:\n");
+			System.out.print("2.Exit\n")
+			choice =scan.next().charAt(0);
+			switch(choice)
+			{
+				case '1': System.out.print("Enter two numbers:\n ")
+					  a=scan.nextFloat();
+					  b=scan.nextFloat();
+			 	 	  res = a+b;
+					  System.out.println("Result = "res\n);
+					  break;
+				case '2': System.exit(0);
+					  break;	
+				default : System.out.println("Invalid Choice!");
+					  break;
+			}
+			System.out.print("\n-----------------\n");
+		}while(choice !=2);
+	}
+}
